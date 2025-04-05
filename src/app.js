2dd30baffbdbf8c0c6adbeaa6301c2f05a27364e
Asjal Amir <mohdasjal1@gmail.com>
2025-04-05 12:08:13 +0500

    dashboard added.

--- src/app.js
+++ src/app.js
@@ -20,12 +20,13 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 import likeRouter from './routes/like.routes.js';
 import commentRouter from "./routes/comment.routes.js";
-
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // http://localhost:8000/api/v1/users/register
 
