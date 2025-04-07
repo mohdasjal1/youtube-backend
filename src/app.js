29b12f85dd47f50d966d356ce69aed5442e709de
Asjal Amir <mohdasjal1@gmail.com>
2025-04-07 23:51:46 +0500

    Add tweet functionality with CRUD operations and routes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import likeRouter from './routes/like.routes.js';
 import commentRouter from "./routes/comment.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import playlistRouter from "./routes/playlist.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -29,6 +30,7 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/tweet", tweetRouter)
 
 // http://localhost:8000/api/v1/users/register
 
