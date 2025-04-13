8cea4430b0be9eae6b4f8a92cc176fe3f229c9b6
Asjal Amir <mohdasjal1@gmail.com>
2025-04-13 16:40:33 +0500

    Refactor video model schema to use nested objects for videoFile and thumbnail, and update default values for views and isPublished

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,54 +1,107 @@
-import mongoose, {Schema} from "mongoose";
+// import mongoose, {Schema} from "mongoose";
+// import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+// const videoSchema = new Schema(
+//     {
+//         videoFile: {
+//         type: String, //cloudinary
+//         required: true,
+//         },
+
+//         thumbnail: {
+//             type: String, //cloudinary
+//             required: true,
+//         },
+
+//         title: {
+//             type: String, 
+//             required: true,
+//         },
+
+//         description: {
+//             type: String, 
+//             required: true,
+//         },
+
+//         duration: {
+//             type: String, // cloudinary
+//             required: true,
+//         },
+
+//         views: {
+//             type: Number,
+//             default: 0
+//         },
+
+//         isPublished: {
+//             type: Boolean,
+//             default: true
+//         },
+
+//         owner: {
+//             type: Schema.Types.ObjectId,
+//             ref: "User"
+//         }
+
+//     },
+//     {
+//         timestamps: true
+//     }
+// )
+
+// videoSchema.plugin(mongooseAggregatePaginate) 
+
+// export const Video = mongoose.model("Video", videoSchema)
+
+
+import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
         videoFile: {
-        type: String, //cloudinary
-        required: true,
+            type: {
+                url: String,
+                public_id: String,
+            },
+            required: true,
         },
-
         thumbnail: {
-            type: String, //cloudinary
+            type: {
+                url: String,
+                public_id: String,
+            },
             required: true,
         },
-
-        title: {
-            type: String, 
-            required: true,
-        },
-
-        description: {
-            type: String, 
-            required: true,
-        },
-
-        duration: {
-            type: String, // cloudinary
-            required: true,
-        },
-
-        views: {
-            type: Number,
-            default: 0
-        },
-
-        isPublished: {
-            type: Boolean,
-            default: true
-        },
-
         owner: {
             type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-
+            ref: "User",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            defaultValue: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            defaultValue: false,
+        },
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-videoSchema.plugin(mongooseAggregatePaginate) 
-
-export const Video = mongoose.model("Video", videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = model("Video", videoSchema);
