99481f2812ba145fbce9135bef28f1366908bc45
Asjal Amir <mohdasjal1@gmail.com>
2025-04-13 07:55:37 +0500

    Refactor asyncHandler to simplify error handling structure

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,23 +1,23 @@
-// const asyncHandler = (requestHandler) => {
-//     return async (req, res, next) => {
-//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-//     }
-// }
-
 const asyncHandler = (requestHandler) => {
     return async (req, res, next) => {
-        try {
-            await Promise.resolve(requestHandler(req, res, next));
-        } catch (error) {
-            const statusCode = error.statusCode || 500;
-            const errorMessage = error.message || "Internal Server Error";
-            res.status(statusCode).json({
-                success: false,
-                error: errorMessage
-            });
-        }
-    };
-};
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+// const asyncHandler = (requestHandler) => {
+//     return async (req, res, next) => {
+//         try {
+//             await Promise.resolve(requestHandler(req, res, next));
+//         } catch (error) {
+//             const statusCode = error.statusCode || 500;
+//             const errorMessage = error.message || "Internal Server Error";
+//             res.status(statusCode).json({
+//                 success: false,
+//                 error: errorMessage
+//             });
+//         }
+//     };
+// };
 
 export { asyncHandler }
 
