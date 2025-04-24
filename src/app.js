00039e6a7cdef0f418e00616285618309237c225
Asjal Amir <mohdasjal1@gmail.com>
2025-04-24 16:49:50 +0500

    Refactor CORS middleware to support multiple allowed origins

--- src/app.js
+++ src/app.js
@@ -5,10 +5,28 @@ import morgan from "morgan";
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        // allow requests with no origin (e.g., Postman, curl)
+        if (!origin) return callback(null, true);
+  
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        } else {
+          return callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
+    })
+  );
+
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
