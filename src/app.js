31952dfa88891ca029fb7e7f1749459ffdbd6ee4
Asjal Amir <mohdasjal1@gmail.com>
2025-04-26 23:25:12 +0500

    Refactor CORS middleware to improve allowedOrigins handling

--- src/app.js
+++ src/app.js
@@ -5,28 +5,28 @@ import morgan from "morgan";
 
 const app = express()
 
-// const allowedOrigins = process.env.CORS_ORIGIN?.split(",").map(origin => origin.trim())
+const allowedOrigins = process.env.CORS_ORIGIN?.split(",").map(origin => origin.trim() || [])
 
-// app.use(
-//     cors({
-//       origin: function (origin, callback) {
-//         // allow requests with no origin (e.g., Postman, curl)
-//         if (!origin) return callback(null, true);
+app.use(
+    cors({
+      origin: function (origin, callback) {
+        // allow requests with no origin (e.g., Postman, curl)
+        if (!origin) return callback(null, true);
   
-//         if (allowedOrigins.includes(origin)) {
-//           return callback(null, true);
-//         } else {
-//           return callback(new Error("Not allowed by CORS"));
-//         }
-//       },
-//       credentials: true,
-//     })
-//   );
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        } else {
+          return callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true,
+    })
+  );
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
