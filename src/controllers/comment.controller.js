d8fa7284fe3957cdba17396911db2bc2f1c2053d
Asjal Amir <mohdasjal1@gmail.com>
2025-04-30 22:41:44 +0500

    update getVideoComments and getUserTweets to project toggle dislikes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,11 +2,13 @@ import mongoose, {Schema} from "mongoose";
 import { Comment } from "../models/comment.model.js"
 import { Video } from "../models/video.model.js";
 import { Like } from "../models/like.model.js";
+import { Dislike } from "../models/dislike.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 // get all comments for a video
+
 const getVideoComments = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     const { page = 1, limit = 10 } = req.query;
@@ -40,16 +42,28 @@ const getVideoComments = asyncHandler(async (req, res) => {
             }
         },
         {
+            $lookup: {
+                from: "dislikes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "dislikes"
+            }
+        },
+        {
             $addFields: {
-                likesCount: {
-                    $size: "$likes"
-                },
-                owner: {
-                    $first: "$owner"
-                },
+                likesCount: { $size: "$likes" },
+                dislikesCount: { $size: "$dislikes" },
+                owner: { $first: "$owner" },
                 isLiked: {
                     $cond: {
                         if: { $in: [req.user?._id, "$likes.likedBy"] },
+                        then: true,
+                        else: false
+                    }
+                },
+                isDisliked: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$dislikes.dislikedBy"] },
                         then: true,
                         else: false
                     }
@@ -66,12 +80,14 @@ const getVideoComments = asyncHandler(async (req, res) => {
                 content: 1,
                 createdAt: 1,
                 likesCount: 1,
+                dislikesCount: 1,
+                isLiked: 1,
+                isDisliked: 1,
                 owner: {
                     username: 1,
                     fullName: 1,
                     avatar: 1
-                },
-                isLiked: 1
+                }
             }
         }
     ]);
@@ -81,15 +97,97 @@ const getVideoComments = asyncHandler(async (req, res) => {
         limit: parseInt(limit, 10)
     };
 
-    const comments = await Comment.aggregatePaginate(
-        commentsAggregate,
-        options
-    );
+    const comments = await Comment.aggregatePaginate(commentsAggregate, options);
 
     return res
         .status(200)
         .json(new ApiResponse(200, comments, "Comments fetched successfully"));
 });
+
+
+// const getVideoComments = asyncHandler(async (req, res) => {
+//     const { videoId } = req.params;
+//     const { page = 1, limit = 10 } = req.query;
+
+//     const video = await Video.findById(videoId);
+
+//     if (!video) {
+//         throw new ApiError(404, "Video not found");
+//     }
+
+//     const commentsAggregate = Comment.aggregate([
+//         {
+//             $match: {
+//                 video: new mongoose.Types.ObjectId(videoId)
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: "users",
+//                 localField: "owner",
+//                 foreignField: "_id",
+//                 as: "owner"
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: "likes",
+//                 localField: "_id",
+//                 foreignField: "comment",
+//                 as: "likes"
+//             }
+//         },
+//         {
+//             $addFields: {
+//                 likesCount: {
+//                     $size: "$likes"
+//                 },
+//                 owner: {
+//                     $first: "$owner"
+//                 },
+//                 isLiked: {
+//                     $cond: {
+//                         if: { $in: [req.user?._id, "$likes.likedBy"] },
+//                         then: true,
+//                         else: false
+//                     }
+//                 }
+//             }
+//         },
+//         {
+//             $sort: {
+//                 createdAt: -1
+//             }
+//         },
+//         {
+//             $project: {
+//                 content: 1,
+//                 createdAt: 1,
+//                 likesCount: 1,
+//                 owner: {
+//                     username: 1,
+//                     fullName: 1,
+//                     avatar: 1
+//                 },
+//                 isLiked: 1
+//             }
+//         }
+//     ]);
+
+//     const options = {
+//         page: parseInt(page, 10),
+//         limit: parseInt(limit, 10)
+//     };
+
+//     const comments = await Comment.aggregatePaginate(
+//         commentsAggregate,
+//         options
+//     );
+
+//     return res
+//         .status(200)
+//         .json(new ApiResponse(200, comments, "Comments fetched successfully"));
+// });
 
 // add a comment to a video
 const addComment = asyncHandler(async (req, res) => {
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -92,6 +92,88 @@ const deleteTweet = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {tweetId}, "Tweet deleted successfully"));
 });
 
+// const getUserTweets = asyncHandler(async (req, res) => {
+//     const { userId } = req.params;
+
+//     if (!isValidObjectId(userId)) {
+//         throw new ApiError(400, "Invalid userId");
+//     }
+
+//     const tweets = await Tweet.aggregate([
+//         {
+//             $match: {
+//                 owner: new mongoose.Types.ObjectId(userId),
+//             },
+//         },
+//         {
+//             $lookup: {
+//                 from: "users",
+//                 localField: "owner",
+//                 foreignField: "_id",
+//                 as: "ownerDetails",
+//                 pipeline: [
+//                     {
+//                         $project: {
+//                             username: 1,
+//                             "avatar.url": 1,
+//                         },
+//                     },
+//                 ],
+//             },
+//         },
+//         {
+//             $lookup: {
+//                 from: "likes",
+//                 localField: "_id",
+//                 foreignField: "tweet",
+//                 as: "likeDetails",
+//                 pipeline: [
+//                     {
+//                         $project: {
+//                             likedBy: 1,
+//                         },
+//                     },
+//                 ],
+//             },
+//         },
+//         {
+//             $addFields: {
+//                 likesCount: {
+//                     $size: "$likeDetails",
+//                 },
+//                 ownerDetails: {
+//                     $first: "$ownerDetails",
+//                 },
+//                 isLiked: {
+//                     $cond: {
+//                         if: {$in: [req.user?._id, "$likeDetails.likedBy"]},
+//                         then: true,
+//                         else: false
+//                     }
+//                 }
+//             },
+//         },
+//         {
+//             $sort: {
+//                 createdAt: -1
+//             }
+//         },
+//         {
+//             $project: {
+//                 content: 1,
+//                 ownerDetails: 1,
+//                 likesCount: 1,
+//                 createdAt: 1,
+//                 isLiked: 1
+//             },
+//         },
+//     ]);
+
+//     return res
+//         .status(200)
+//         .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
+// });
+
 const getUserTweets = asyncHandler(async (req, res) => {
     const { userId } = req.params;
 
@@ -137,21 +219,40 @@ const getUserTweets = asyncHandler(async (req, res) => {
             },
         },
         {
+            $lookup: {
+                from: "dislikes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "dislikeDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            dislikedBy: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
             $addFields: {
-                likesCount: {
-                    $size: "$likeDetails",
-                },
-                ownerDetails: {
-                    $first: "$ownerDetails",
-                },
+                likesCount: { $size: "$likeDetails" },
+                dislikesCount: { $size: "$dislikeDetails" },
+                ownerDetails: { $first: "$ownerDetails" },
                 isLiked: {
                     $cond: {
-                        if: {$in: [req.user?._id, "$likeDetails.likedBy"]},
+                        if: { $in: [req.user?._id, "$likeDetails.likedBy"] },
+                        then: true,
+                        else: false
+                    }
+                },
+                isDisliked: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$dislikeDetails.dislikedBy"] },
                         then: true,
                         else: false
                     }
                 }
-            },
+            }
         },
         {
             $sort: {
@@ -161,18 +262,21 @@ const getUserTweets = asyncHandler(async (req, res) => {
         {
             $project: {
                 content: 1,
+                createdAt: 1,
                 ownerDetails: 1,
                 likesCount: 1,
-                createdAt: 1,
-                isLiked: 1
-            },
-        },
+                dislikesCount: 1,
+                isLiked: 1,
+                isDisliked: 1
+            }
+        }
     ]);
 
     return res
         .status(200)
         .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
 });
+
 
 export { createTweet, updateTweet, deleteTweet, getUserTweets };
 
