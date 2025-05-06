24e00b4cdc559a84fd6cb6c31f61aa87cdc3b71c
Asjal Amir <mohdasjal1@gmail.com>
2025-05-06 11:58:11 +0500

    Refactor addComment to return structured response data and improve owner population

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -104,92 +104,6 @@ const getVideoComments = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, comments, "Comments fetched successfully"));
 });
 
-
-// const getVideoComments = asyncHandler(async (req, res) => {
-//     const { videoId } = req.params;
-//     const { page = 1, limit = 10 } = req.query;
-
-//     const video = await Video.findById(videoId);
-
-//     if (!video) {
-//         throw new ApiError(404, "Video not found");
-//     }
-
-//     const commentsAggregate = Comment.aggregate([
-//         {
-//             $match: {
-//                 video: new mongoose.Types.ObjectId(videoId)
-//             }
-//         },
-//         {
-//             $lookup: {
-//                 from: "users",
-//                 localField: "owner",
-//                 foreignField: "_id",
-//                 as: "owner"
-//             }
-//         },
-//         {
-//             $lookup: {
-//                 from: "likes",
-//                 localField: "_id",
-//                 foreignField: "comment",
-//                 as: "likes"
-//             }
-//         },
-//         {
-//             $addFields: {
-//                 likesCount: {
-//                     $size: "$likes"
-//                 },
-//                 owner: {
-//                     $first: "$owner"
-//                 },
-//                 isLiked: {
-//                     $cond: {
-//                         if: { $in: [req.user?._id, "$likes.likedBy"] },
-//                         then: true,
-//                         else: false
-//                     }
-//                 }
-//             }
-//         },
-//         {
-//             $sort: {
-//                 createdAt: -1
-//             }
-//         },
-//         {
-//             $project: {
-//                 content: 1,
-//                 createdAt: 1,
-//                 likesCount: 1,
-//                 owner: {
-//                     username: 1,
-//                     fullName: 1,
-//                     avatar: 1
-//                 },
-//                 isLiked: 1
-//             }
-//         }
-//     ]);
-
-//     const options = {
-//         page: parseInt(page, 10),
-//         limit: parseInt(limit, 10)
-//     };
-
-//     const comments = await Comment.aggregatePaginate(
-//         commentsAggregate,
-//         options
-//     );
-
-//     return res
-//         .status(200)
-//         .json(new ApiResponse(200, comments, "Comments fetched successfully"));
-// });
-
-// add a comment to a video
 const addComment = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
     const { content } = req.body;
@@ -199,7 +113,6 @@ const addComment = asyncHandler(async (req, res) => {
     }
 
     const video = await Video.findById(videoId);
-
     if (!video) {
         throw new ApiError(404, "Video not found");
     }
@@ -214,17 +127,75 @@ const addComment = asyncHandler(async (req, res) => {
         throw new ApiError(500, "Failed to add comment please try again");
     }
 
-    comment = await Comment.populate({
+    // Populate only the avatar field of the owner
+    comment = await comment.populate({
         path: "owner",
         select: "username fullName avatar"
-    })
+    });
+
+    // Structure response same as other comment-fetching routes
+    const responseData = {
+        _id: comment._id,
+        content: comment.content,
+        createdAt: comment.createdAt,
+        likesCount: 0,
+        dislikesCount: 0,
+        isLiked: false,
+        isDisliked: false,
+        owner: {
+            username: comment.owner.username,
+            fullName: comment.owner.fullName,
+            avatar: comment.owner.avatar
+        }
+    };
 
     return res
         .status(201)
-        .json(new ApiResponse(201, comment, "Comment added successfully"));
+        .json(new ApiResponse(201, responseData, "Comment added successfully"));
 });
 
+
+
+
+// add a comment to a video
+
+
+// const addComment = asyncHandler(async (req, res) => {
+//     const { videoId } = req.params;
+//     const { content } = req.body;
+
+//     if (!content) {
+//         throw new ApiError(400, "Content is required");
+//     }
+
+//     const video = await Video.findById(videoId);
+
+//     if (!video) {
+//         throw new ApiError(404, "Video not found");
+//     }
+
+//     let comment = await Comment.create({
+//         content,
+//         video: videoId,
+//         owner: req.user?._id
+//     });
+
+//     if (!comment) {
+//         throw new ApiError(500, "Failed to add comment please try again");
+//     }
+
+//     comment = await Comment.populate({
+//         path: "owner",
+//         select: "username fullName avatar"
+//     })
+
+//     return res
+//         .status(201)
+//         .json(new ApiResponse(201, comment, "Comment added successfully"));
+// });
+
 // update a comment
+
 const updateComment = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
     const { content } = req.body;
